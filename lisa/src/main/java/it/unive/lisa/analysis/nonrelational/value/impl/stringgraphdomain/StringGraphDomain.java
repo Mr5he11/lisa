7e51910d00acfce9ec81c285957165521c2e7bec
Davide Volpe <volpe.davide.vd@gmail.com>
2021-04-16 15:15:22 +0200

    specified generic for StringGraphDomain; gitignore modified for Eclipse

--- lisa/src/main/java/it/unive/lisa/analysis/nonrelational/value/impl/stringgraphdomain/StringGraphDomain.java
+++ lisa/src/main/java/it/unive/lisa/analysis/nonrelational/value/impl/stringgraphdomain/StringGraphDomain.java
@@ -1,88 +1,56 @@
 package it.unive.lisa.analysis.nonrelational.value.impl.stringgraphdomain;
 
-import it.unive.lisa.analysis.BaseLattice;
-import it.unive.lisa.analysis.Lattice;
-import it.unive.lisa.analysis.SemanticDomain;
 import it.unive.lisa.analysis.SemanticException;
-import it.unive.lisa.analysis.lattices.FunctionalLattice;
-import it.unive.lisa.analysis.nonrelational.NonRelationalDomain;
 import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
-import it.unive.lisa.program.cfg.ProgramPoint;
-import it.unive.lisa.symbolic.SymbolicExpression;
 
-public class StringGraphDomain extends BaseNonRelationalValueDomain {
-    @Override
-    protected BaseLattice lubAux(BaseLattice other) throws SemanticException {
-        return null;
-    }
+public class StringGraphDomain extends BaseNonRelationalValueDomain<StringGraphDomain> {
 
-    @Override
-    protected BaseLattice wideningAux(BaseLattice other) throws SemanticException {
-        return null;
-    }
+	@Override
+	public String representation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    protected boolean lessOrEqualAux(BaseLattice other) throws SemanticException {
-        return false;
-    }
+	@Override
+	public StringGraphDomain top() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        return false;
-    }
+	@Override
+	public StringGraphDomain bottom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public int hashCode() {
-        return 0;
-    }
+	@Override
+	protected StringGraphDomain lubAux(StringGraphDomain other) throws SemanticException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public Lattice lub(Lattice other) throws SemanticException {
-        return null;
-    }
+	@Override
+	protected StringGraphDomain wideningAux(StringGraphDomain other) throws SemanticException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public Lattice widening(Lattice other) throws SemanticException {
-        return null;
-    }
+	@Override
+	protected boolean lessOrEqualAux(StringGraphDomain other) throws SemanticException {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public boolean lessOrEqual(Lattice other) throws SemanticException {
-        return false;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public Lattice top() {
-        return null;
-    }
-
-    @Override
-    public Lattice bottom() {
-        return null;
-    }
-
-    @Override
-    public NonRelationalDomain eval(SymbolicExpression expression, FunctionalLattice environment, ProgramPoint pp) throws SemanticException {
-        return null;
-    }
-
-    @Override
-    public SemanticDomain.Satisfiability satisfies(SymbolicExpression expression, FunctionalLattice environment, ProgramPoint pp) throws SemanticException {
-        return null;
-    }
-
-    @Override
-    public FunctionalLattice assume(FunctionalLattice environment, SymbolicExpression expression, ProgramPoint pp) throws SemanticException {
-        return null;
-    }
-
-    @Override
-    public NonRelationalDomain glb(NonRelationalDomain other) throws SemanticException {
-        return null;
-    }
-
-    @Override
-    public String representation() {
-        return null;
-    }
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+    
 }
